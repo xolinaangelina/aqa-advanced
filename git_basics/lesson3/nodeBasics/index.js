78e1244ba1b05d3428f7cb4dec19d7c2af8bcf46
xolinaangelina <xolinaangelina15@gmail.com>
2026-04-19 15:55:44 +0300

    feat: add nodeBasics with chalk colored output

---
+++ git_basics/lesson3/nodeBasics/index.js
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Привіт, синій текст!'));
+console.log(chalk.red.bold('Це червоний жирний текст!'));
+console.log(chalk.green('Зелений текст!'));
+console.log(chalk.yellow.bgBlack('Жовтий на чорному фоні'));
+console.log(chalk.magenta.underline('Підкреслений фіолетовий'));
